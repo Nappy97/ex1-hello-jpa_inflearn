54ab2b93c3ce3c8fb17b6160436cfccc11dec2b3
Nappy97 <wjddmlgud829@gmail.com>
2022-12-25 01:33:46 +0900

    1225

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,8 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class JpaMain {
 
@@ -175,15 +173,65 @@ public class JpaMain {
 //            Item item = em.find(Item.class, movie.getId());
 //            System.out.println("findMovie = " + item);
 
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
+//            Member member = new Member();
+//            member.setUsername("user1");
+//            member.setCreatedBy("kim");
+//            member.setCreatedDate(LocalDateTime.now());
+//
+//            em.persist(member);
+//
+//            em.flush();
+//            em.clear();
 
-            em.persist(member);
+            
+//
+//            Member member = em.find(Member.class, 1L);
+//            printMember(member);
+//            printMemberAndTeam(member);
+//
+//            Member member = new Member();
+//            member.setUsername("hello");
+//
+//            em.persist(member);
+//
+//            em.flush();
+//            em.clear();
+
+            //
+//            Member findMember = em.find(Member.class, member.getId());
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.username = " + findMember.getUsername());
+
+            // 프록시
+//            Member findMember = em.getReference(Member.class, member.getId());
+//            System.out.println("findMember = " + findMember.getClass());
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.username = " + findMember.getUsername());
+
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
+
+//            Member member2 = new Member();
+//            member2.setUsername("member2");
+//            em.persist(member2);
 
             em.flush();
             em.clear();
+
+            Member m1 = em.getReference(Member.class, member1.getId());
+//            Member m2 = em.getReference(Member.class, member2.getId());
+
+//            logic(m1, m2);
+
+            System.out.println("m1 = " + m1.getClass());
+
+            Member reference = em.find(Member.class, member1.getId());
+            System.out.println("reference = " + reference.getClass());
+
+            System.out.println("a == a: " + (m1 == reference));
+
 
             tx.commit();
         } catch (Exception e) {
@@ -194,4 +242,21 @@ public class JpaMain {
 
         emf.close();
     }
+
+//    private static void logic(Member m1, Member m2) {
+//        System.out.println("m1 == m2: " + (m1  instanceof Member));
+//        System.out.println("m1 == m2: " + (m2  instanceof Member));
+//    }
+
+//    private static void printMember(Member member) {
+//        System.out.println("member = " + member.getUsername());
+//    }
+//
+//    private static void printMemberAndTeam(Member member) {
+//        String username = member.getUsername();
+//        System.out.println("username = " + username);
+//
+//        Team team = member.getTeam();
+//        System.out.println("team = " + team);
+//    }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -39,12 +39,12 @@ public class Member extends BaseEntity{
 //    @JoinColumn(name = "TEAM_ID")
 //    private Team team;
 
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
+//    @OneToOne
+//    @JoinColumn(name = "LOCKER_ID")
+//    private Locker locker;
+//
+//    @OneToMany(mappedBy = "member")
+//    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -61,5 +61,14 @@ public class Member extends BaseEntity{
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
 
 }
